12886bae922aaddc1657ed88a473528da88c6cf7
N-benitha <ngungabn03@gmail.com>
2025-01-14 17:38:11 -0800

    changes

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 #kills a process named killmenow
 exec {'pkill -f killmenow':
-path   => '/usr/bin/:/usr/local/bin/:/bin/',
-onlyif => 'pgrep -f killmenow',
-unless => 'pgrep -f killmenow'
+command => '/usr/bin/pkill -f killmenow',
+path   	=> '/usr/bin/:/usr/local/bin/:/bin/',
+onlyif 	=> '/usr/bin/pgrep -f killmenow',
 }
