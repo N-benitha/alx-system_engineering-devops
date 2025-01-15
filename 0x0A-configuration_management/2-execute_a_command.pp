3315e6324b0ba745febfbc3822b2d01fb62d48f2
N-benitha <ngungabn03@gmail.com>
2025-01-14 17:47:04 -0800

    indenation errors

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 #kills a process named killmenow
 exec {'pkill -f killmenow':
 command => '/usr/bin/pkill -f killmenow',
-path   	=> '/usr/bin/:/usr/local/bin/:/bin/',
-onlyif 	=> '/usr/bin/pgrep -f killmenow',
+path    => '/usr/bin/:/usr/local/bin/:/bin/',
+onlyif  => '/usr/bin/pgrep -f killmenow',
 }
